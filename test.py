22c5eaaa67f6e7b580ce58e0a3ef4fdf2788b83b
纯情 <476131154@qq.com>
2017-12-29 17:10:41 +0800

    2017.12.29

--- test.py
+++ test.py
@@ -1,7 +1,13 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 #coding=utf-8
-import urllib2
 
-response = urllib2.urlopen("http://www.baidu.com")
-print response.read()
-#zore
+#ASCII码与字符的相互转换
+print( ord('A'), ord('人'), ord('民'), chr(66), chr(44), chr(20154))
+
+
+#second
+# name = input("输入名字:") #输入数字：input()   raw_input 字符串
+# print(name)
+
+#first 
+#print("hello,world")
